76a1066b4ec292dfa519a58348d0e7a910ac89e4
madhulika12 <mt895@msstate.edu>
2014-10-11 20:33:13 -0500

    Works both for Gas and Water

---
+++ modbus_attacks.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# vim: tw=80
+
+from ..protolibs.modbus_tk import utils as modbus_utils
+
+"""
+
+Modbus Attacks
+This module holds attacks against ModbusTCP and ModbusASCII slaves and masters.
+"""
+
+class Attack(object):
+    def runAttack(*args, **kwds):
+        raise NotImplementedError
+
+class ModbusAttack(Attack):
+    pass
+
+class ModbusRTUAttack(Attack):
+    def createPort(filename):
+        """Creates serial port objects with appropriate characteristics"""
+        pass
+    def bytewiseRead(self, ):
+
+
